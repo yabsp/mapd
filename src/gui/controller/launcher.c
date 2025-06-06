f25c4e06f7258d6dfb4e5fa8e9b1644a61f9a68b
mueller-mike <mike.mueller28@outlook.com>
2025-06-06 17:03:48 +0200

    Fixes bung in launcher.c where multiple arguments would result in multiple launches of the application

--- src/gui/controller/launcher.c
+++ src/gui/controller/launcher.c
@@ -8,49 +8,54 @@ gboolean launch_application(const gchar *exec_path, const gchar *arguments)
     // Build LD_PRELOAD, environment and command arguments
     gchar *ld_preload = g_strdup_printf("LD_PRELOAD=%s", memwrap_path);
     gchar *envp[] = { ld_preload, NULL };
-
-    // Prepare full command string: binary + arguments (single string)
-    gchar *full_command = NULL;
-    if (arguments && strlen(arguments) > 0)
-        full_command = g_strdup_printf("%s %s", exec_path, arguments);
-    else
-        full_command = g_strdup(exec_path);
-
-    // Use g_shell_parse_argv to safely split into argv[]
     gchar **argv = NULL;
     GError *parse_error = NULL;
-    if (!g_shell_parse_argv(full_command, NULL, &argv, &parse_error))
-    {
-        g_warning("Argument parsing failed: %s", parse_error->message);
-        g_error_free(parse_error);
-        g_free(full_command);
-        g_free(ld_preload);
-        return FALSE;
+
+    if (arguments && strlen(arguments) > 0)
+	{
+		gchar *command_line = g_strdup_printf("%s %s", exec_path, arguments);
+		if (!g_shell_parse_argv(command_line, NULL, &argv, &parse_error))
+        {
+            g_warning("Argument parsing failed: %s", parse_error->message);
+            g_error_free(parse_error);
+            g_free(command_line);
+            g_free(ld_preload);
+            return FALSE;
+        }
+		g_free(command_line);
     }
+	else
+	{
+        argv = g_new0(gchar *, 2);
+        argv[0] = g_strdup(exec_path);
+        argv[1] = NULL;
+	}
 
     // Launch application
     GError *error = NULL;
     gboolean success = g_spawn_async
     (
-        NULL,    // working directory
-        argv,    // command arguments
-        envp,    // environment variables
-        G_SPAWN_DEFAULT,  // spawn flags
-        NULL, NULL,       // child setup
-        NULL,             // child PID
-        &error            // error
+        NULL,				// working directory
+        argv,				// command arguments
+        envp,				// environment variables
+        G_SPAWN_DEFAULT,	// spawn flags
+        NULL, NULL,			// child setup
+        NULL,             	// child PID
+        &error            	// error
     );
 
     // Check if application launch failed
-    if (!success) {
+    if (!success)
+	{
         g_warning("Failed to launch: %s", error->message);
         g_error_free(error);
         g_free(ld_preload);
         return FALSE;
-    } else {
-        g_print("Launched: %s (with LD_PRELOAD=%s)\n", exec_path, memwrap_path);
     }
+ 	else
+        g_print("Launched: %s (with LD_PRELOAD=%s)\n", exec_path, memwrap_path);
 
+	g_strfreev(argv);
     g_free(ld_preload);
     return TRUE;
 }

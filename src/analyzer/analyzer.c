f6ec470864f159c4404793ab91a16c61b39ab830
mueller-mike <mike.mueller28@outlook.com>
2025-06-08 23:03:18 +0200

    Changes analyzer.c to a library to run as a daemon while gui is running.

--- src/analyzer/analyzer.c
+++ src/analyzer/analyzer.c
@@ -14,6 +14,64 @@
 static int client_counter = 0;
 pthread_mutex_t counter_lock = PTHREAD_MUTEX_INITIALIZER;
 
+static void* server_socket_thread(void* arg)
+{
+    (void)arg;
+
+    int server_fd;
+    struct sockaddr_un addr;
+
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket"); exit(EXIT_FAILURE);
+    }
+
+    unlink(SOCKET_PATH);
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+    {
+        perror("bind"); exit(EXIT_FAILURE);
+    }
+
+    listen(server_fd, 10);
+    printf("[Analyzer] Listening on %s\n", SOCKET_PATH);
+
+    while (1) {
+        int client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        ClientContext* ctx = malloc(sizeof(ClientContext));
+        if (!ctx) {
+            perror("malloc");
+            close(client_fd);
+            continue;
+        }
+
+        ctx->client_fd = client_fd;
+
+        pthread_mutex_lock(&counter_lock);
+        ctx->client_number = ++client_counter;
+        pthread_mutex_unlock(&counter_lock);
+
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, handle_client, ctx) != 0) {
+            perror("pthread_create");
+            close(client_fd);
+            free(ctx);
+        } else {
+            ctx->thread_id = tid;
+            pthread_detach(tid);
+        }
+    }
+}
+
 
 void* handle_client(void* arg) {
     ClientContext* ctx = (ClientContext*)arg;
@@ -46,79 +104,23 @@ void* handle_client(void* arg) {
     return NULL;
 }
 
-
 void* gui_consumer_thread(void* arg) {
-    (void)arg; // unused
+    (void)arg;
     while (1) {
         Message msg = dequeue_message();
         printf("[GUI] Client %d | %-12s | Addr: %-12s | Size: %-5zu | Thread: %lu | Time: %ld\n",
-               msg.client_id, msg.type, msg.addr, msg.size, msg.thread, msg.timestamp);
+            msg.client_id, msg.type, msg.addr, msg.size, msg.thread, msg.timestamp);
     }
     return NULL;
 }
 
-int main() {
-    int server_fd;
-    struct sockaddr_un addr;
-
-    pthread_t gui_thread;
-    pthread_create(&gui_thread, NULL, gui_consumer_thread, NULL);
-    pthread_detach(gui_thread);
-
+void analyzer_init(void)
+{
     pthread_t frag_thread;
     pthread_create(&frag_thread, NULL, fragmentation_thread, NULL);
     pthread_detach(frag_thread);
 
-    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    unlink(SOCKET_PATH);
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
-
-    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
-
-    listen(server_fd, 10);
-    printf("[Analyzer] Listening on %s\n", SOCKET_PATH);
-
-    while (1) {
-        int client_fd = accept(server_fd, NULL, NULL);
-        if (client_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        ClientContext* ctx = malloc(sizeof(ClientContext));
-        if (!ctx) {
-            perror("malloc");
-            close(client_fd);
-            continue;
-        }
-
-        ctx->client_fd = client_fd;
-        pthread_mutex_lock(&counter_lock);
-        ctx->client_number = ++client_counter;
-        pthread_mutex_unlock(&counter_lock);
-
-        pthread_t tid;
-        if (pthread_create(&tid, NULL, handle_client, ctx) != 0) {
-            perror("pthread_create");
-            close(client_fd);
-            free(ctx);
-        } else {
-            ctx->thread_id = tid;
-            pthread_detach(tid);
-        }
-    }
-
-    close(server_fd);
-    unlink(SOCKET_PATH);
-    return 0;
+    pthread_t server_thread;
+    pthread_create(&server_thread, NULL, server_socket_thread, NULL);
+    pthread_detach(server_thread);
 }
--- src/analyzer/analyzer.h
+++ src/analyzer/analyzer.h
@@ -2,12 +2,15 @@
 #define ANALYZER_H
 
 #include <pthread.h>
+#include <message.h>
 
 typedef struct {
     int client_fd;
     pthread_t thread_id;
     int client_number;
 } ClientContext;
+
+void analyzer_init(void);
 
 void* handle_client(void* arg);
 
